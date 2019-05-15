150732345b2a6483d3dbdc972552da7227dd0582
Hoanh An <hoanhan@bennington.edu>
2019-05-14 23:52:06 -0400

    update payment processor

---
+++ payment_processor/api.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+
+class Payment_Processor(object):
+    def setup(self):
+        print("payment_processor is setting up")
+
+    def send_invoice(self, card, items):
+        print("payment_processor is sending invoice of", items, "to", card)
