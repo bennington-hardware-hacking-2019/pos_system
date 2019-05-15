7c8421b73a097487b0f465b8b9378d5b58973675
Five Grant <5@fivegrant.com>
2019-05-15 18:56:25 -0400

    added wiegand reader functionality to Controller. Wiegand = prx_3hc so there's no more of that. ONLY WIEGAND. Also, I added the get_card command to the controller which demonstrates the errors of the wiegand reader objects. I don't necessarily think that function will stick around.

--- controller/api.py
+++ controller/api.py
@@ -5,14 +5,14 @@ import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import pn532
-import prx_3hc
+import wiegand
 import store
 import payment_processor
 
 
 class Controller(object):
     def __init__(self):
-        self.prx_3hc = prx_3hc.PRX_3HC()
+        self.wiegand = wiegand.Wiegand26()
         self.pn532 = pn532.PN532()
         self.store = store.Store()
         self.payment_processor = payment_processor.Payment_Processor()
@@ -20,10 +20,24 @@ class Controller(object):
     def setup(self):
         print("controller is setting up")
 
-        self.prx_3hc.setup()
+        self.wiegand.setup()
         self.pn532.setup()
         self.store.setup()
         self.payment_processor.setup()
+
+    
+    def get_card(self):
+        """function handles exceptions. this will probably be deleted later
+           but it illustrates how the errors work
+        """
+        try:
+            return self.wiegand.read()
+        except FacilityError:
+            print("Please use a Bennington Card")
+            return self.get_card()
+        except ReadError:
+            print("Read Error: Please try again")
+            return self.get_card()
 
     def run(self):
         """run the system sequentially (only for demo purposes)
@@ -32,7 +46,10 @@ class Controller(object):
         print("controller is running")
 
         # tap a bennington card to start a check out session
-        bennington_card = self.prx_3hc.read()
+
+        bennington_card = self.get_card()
+        # What was here before ^^^^
+        ##bennington_card = self.wiegand.read()
 
         # if the card is invalid (not in the store), end the session
         if not self.store.validate_card(bennington_card):
@@ -45,7 +62,7 @@ class Controller(object):
 
         # tap the bennington card again to end the checkout session
         # if the card is not the same card as before, end the session
-        if not bennington_card == self.prx_3hc.read():
+        if not bennington_card == self.wiegand.read():
             return False
 
         # get informations for items in the card and update their status
