44f547cb802911033ba1310382412dd6f16f75ad
Hoanh An <hoanhan@bennington.edu>
2019-05-20 01:35:52 +0000

    test on real hardware successfully

--- controller/api.py
+++ controller/api.py
@@ -6,7 +6,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 
 import pn532
 import wiegand
-import store
+import db
 import payment_processor
 
 
@@ -33,46 +33,54 @@ class Controller(object):
 
         # tap a bennington card to start a check out session
         bennington_card = self.wiegand.read()
+        print(bennington_card)
 
-        # if the card is invalid (not in the db), end the session
-        if not self.db.validate_card(bennington_card):
-            return False
-
-        # scan a number of nfc tags and add them to a cart
-        # FIXME - run sequentially, get 3 items for now
-        cart = []
-        for i in range(3):
-            cart.append(self.pn532.read())
-
-        # tap the bennington card again to end the checkout session
-        # if the card is not the same card as before, end the session
-        if not bennington_card == self.wiegand.read():
-            return False
-
-        # get informations for available items in the cart
-        items = self.db.get_available_items(cart)
-
-        if len(items) == 0:
-            return False
-
-        # send an invoice to the person
-        if not self.payment_processor.send_invoice(bennington_card, items):
-            return False
-
-        # create a pending transaction
-        # TODO - need to verify transaction once it's paid then update the
-        # database again
-        transaction_id = self.db.create_pending_transaction(bennington_card, items)
-
-        # print out the transaction information
-        print("transaction:", self.db.get_transaction(transaction_id))
-
-        # update items status to sold
-        self.db.update_sold_items(items)
-
-        # check the availability of the item in the cart again
-        # since all of them are sold, there should be no item left to be purchased
-        if len(self.db.get_available_items(cart)) != 0:
-            return False
+        nfc_tag = self.pn532.read()
+        print(nfc_tag)
 
         return True
+
+        # FIXME - comment out the below part to test the real hardware
+
+        # # if the card is invalid (not in the db), end the session
+        # if not self.db.validate_card(bennington_card):
+        #     return False
+
+        # # scan a number of nfc tags and add them to a cart
+        # # FIXME - run sequentially, get 3 items for now
+        # cart = []
+        # for i in range(3):
+        #     cart.append(self.pn532.read())
+
+        # # tap the bennington card again to end the checkout session
+        # # if the card is not the same card as before, end the session
+        # if not bennington_card == self.wiegand.read():
+        #     return False
+
+        # # get informations for available items in the cart
+        # items = self.db.get_available_items(cart)
+
+        # if len(items) == 0:
+        #     return False
+
+        # # send an invoice to the person
+        # if not self.payment_processor.send_invoice(bennington_card, items):
+        #     return False
+
+        # # create a pending transaction
+        # # TODO - need to verify transaction once it's paid then update the
+        # # database again
+        # transaction_id = self.db.create_pending_transaction(bennington_card, items)
+
+        # # print out the transaction information
+        # print("transaction:", self.db.get_transaction(transaction_id))
+
+        # # update items status to sold
+        # self.db.update_sold_items(items)
+
+        # # check the availability of the item in the cart again
+        # # since all of them are sold, there should be no item left to be purchased
+        # if len(self.db.get_available_items(cart)) != 0:
+        #     return False
+
+        # return True
