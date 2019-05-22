57da9ff5168721109bafb136a74b9e6947da5ee7
Hoanh An <hoanhan@bennington.edu>
2019-05-22 05:40:17 +0000

    update real reading to db

--- db/data.sql
+++ db/data.sql
@@ -1,15 +1,17 @@
 /* pre-populated data */
 
-INSERT INTO "buyer" VALUES (DEFAULT, 28640, 10000, 'hoanh', 'hoanhan@bennington.edu');
+INSERT INTO "buyer" VALUES (DEFAULT, 28640, 7114, 'hoanh', 'hoanhan@bennington.edu');
 
 INSERT INTO "admin" VALUES (DEFAULT, 28640, TRUE);
 
-INSERT INTO "item" VALUES (DEFAULT, '{100,0,0}', 'sample item 1', 'sample description 1',  1.11, DEFAULT, NULL);
-INSERT INTO "item" VALUES (DEFAULT, '{200,0,0}', 'sample item 2', 'sample description 2',  2.22, DEFAULT, NULL);
-INSERT INTO "item" VALUES (DEFAULT, '{300,0,0}', 'sample item 3', 'sample description 3',  3.33, DEFAULT, NULL);
-INSERT INTO "item" VALUES (DEFAULT, '{400,0,0}', 'sample item 4', 'sample description 4',  4.44, DEFAULT, NULL);
-INSERT INTO "item" VALUES (DEFAULT, '{500,0,0}', 'sample item 5', 'sample description 5',  5.55, DEFAULT, NULL);
+INSERT INTO "item" VALUES (DEFAULT, '{1,0,68,0,7,4,137,16,98,101,96}', 'sample item 1', 'sample description 1',  1.11, DEFAULT, NULL);
+INSERT INTO "item" VALUES (DEFAULT, '{1,0,68,0,7,4,187,16,122,101,96}', 'sample item 2', 'sample description 2',  2.22, DEFAULT, NULL);
+INSERT INTO "item" VALUES (DEFAULT, '{1,0,68,0,7,4,53,205,106,101,96}', 'sample item 3', 'sample description 3',  3.33, DEFAULT, NULL);
+INSERT INTO "item" VALUES (DEFAULT, '{1,0,68,0,7,4,29,208,106,101,96}', 'sample item 4', 'sample description 4',  4.44, DEFAULT, NULL);
+INSERT INTO "item" VALUES (DEFAULT, '{1,0,68,0,7,4,15,206,106,101,96}', 'sample item 5', 'sample description 5',  5.55, DEFAULT, NULL);
 
 INSERT INTO "stock" VALUES (DEFAULT, 1);
+INSERT INTO "stock" VALUES (DEFAULT, 2);
 INSERT INTO "stock" VALUES (DEFAULT, 3);
+INSERT INTO "stock" VALUES (DEFAULT, 4);
 INSERT INTO "stock" VALUES (DEFAULT, 5);
