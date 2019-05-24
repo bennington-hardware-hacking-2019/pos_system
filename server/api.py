bcb88591f1472055fac70493b6b1793701b0d67b
Matthew Collyer <matthewcollyer@bennington.edu>
2019-05-23 22:20:35 -0400

    Cleaned up Flask, got rid of old code. -changed html to html.j2 -changed checkout_here thing to cart -TABS>SPACES - changed info to about - removed cart function ... old code - changed admin to stock - changed inventory to stock - changed create to add - updated db function name ...  old code - yo

--- server/api.py
+++ server/api.py
@@ -14,9 +14,7 @@ class Server(object):
 		self.app = Flask(__name__)
 		self.app.secret_key = 'yagabeatsTHO'
 		self.socketio = SocketIO(self.app)
-
 		self.is_read = False
-
 		self.tag_reader = tag_reader.PN532()
 		self.card_reader = card_reader.Wiegand()
 		self.db = db.DB()
@@ -35,10 +33,10 @@ class Server(object):
 
 		@self.app.route('/')
 		def index():
-			return render_template("base.html.j2", variable="Oy")
+			return render_template("base.html.j2")
 
-		@self.app.route('/proceed_to_checkout')
-		def proceed_to_checkout():
+		@self.app.route('/cart')
+		def cart():
 			return "checkout here"
 
 		@self.socketio.on('server request')
@@ -54,118 +52,52 @@ class Server(object):
 
 			item = self.db.get_item(tag)
 			resp = {
-				'item': item.get('item'),
-				'description': item.get('description'),
-				'cost': item.get('cost')
-			}
+					'item': item.get('item'),
+					'description': item.get('description'),
+					'cost': item.get('cost')
+					}
 
 			emit('server response', resp)
 
 		@self.app.route('/help')
 		def help():
-			return render_template('help.html')
+			return render_template('help.html.j2')
 
-		@self.app.route('/info')
+		@self.app.route('/about')
 		def info():
-			return render_template('info.html')
+			return render_template('about.html.j2')
 
 		@self.app.route('/login', methods = ['GET'])
 		def login():
-			return render_template('login.html')
+			return render_template('login.html.j2')
 
-		@self.app.route('/admin', methods = ['POST'])
-		def admin():
+		@self.app.route('/stock', methods = ['POST'])
+		def stock():
 			#postgres for card - long term
 			#get the pin from the login form - temporary
 			pin = int(request.form['pin'])
-
 			#if password/card verified
-
 			if (pin == 123):
 				session['username'] = 'admin' #Temporary
-				return render_template('inventory.html')
+				stock = self.db.get_stock()
+				return render_template('stock.html.j2',stock = stock)
 			else:
 				return redirect(url_for('login')) #needs testing
 
-		@self.app.route('/admin/create', methods = ['GET', 'POST'])
-		def create():
+		@self.app.route('/stock/add', methods = ['GET', 'POST'])
+		def add():
 			if 'username' in session:
 				if request.method == 'POST':
 					name = request.form['name']
 					desc = request.form['desc']
 					cost = request.form['cost']
-					#insert this stuff into whatever
-					db.create(name, desc, cost)
-					return redirect(url_for('items'))
+					self.db.add_item(name, desc, cost)
+					return redirect(url_for('admin'))
 				elif request.method == 'GET':
-					return render_template('create.html')
+					return render_template('add.html.j2')
 			else:
 				return redirect(url_for('login')) #needs testing
-
-		#
-		# Vue.js API endpoints
-		#
-		@self.app.route('/items', methods = ['GET'])
-		def items():
-			#postgres
-			item = db.items()
-			return render_template('data.html', items=items)
 
 	def start(self):
 		# self.app.run(debug=True)
 		self.socketio.run(self.app, debug=True)
-
-	def cart(self, sim=False):
-		"""
-		run the pos_system
-		"""
-
-		# we use a dictionary to ensure the uniqueness of a tag
-		tags = {}
-
-		# cart loop
-		while True:
-			# check for a tag reading
-			tag = self.tags.read()
-
-			# check if the item exists
-			item = self.db.get_item(tag)
-			if item is not None:
-
-				# add the item to the cart
-				tags.append(tag)
-
-				# send the tag's item to the ui
-				self.ui.add_item(item)
-			else:
-				print("That tag can't be found in stock")
-
-		# check for a card reading
-		card = self.card.read()
-
-		if self.db.check_card(card):
-			# collect all the items
-			items = self.db.get_items(tags)
-
-			# make the sale
-			sale = self.db.make_sale(card, tags)
-
-			# update the ui
-			self.ui.checkout(sale, items)
-
-			# send invoice to the customer
-			card_info = self.db.get_buyer(card)
-			name = card_info.get("name")
-			email = card_info.get("email")
-			charge_id = self.payment_processor.send_invoice(name, email, items)
-
-			# print(self.payment_processor.get_charge(charge_id))
-
-			# FIXME - check if it's paid
-			# realistically would check it later
-			if self.payment_processor.is_paid(charge_id):
-				print(name, "has paid")
-
-			# FIXME - add logic to update the stock after an item is purchased
-		else:
-			self.ui.card_error();
