d1de8bd8fd62323967cfb8dcc15197b2a45f0a0e
Julian <julianlambert@bennington.edu>
2019-05-26 19:12:17 -0400

    moved checkout to seperate endpoint

---
+++ server/static/checkout.js
@@ -0,0 +1,39 @@
+var socket = io.connect('http://' + document.domain + ':' + location.port);
+
+// get cart element by id
+var $cart = $('#cart');
+
+// dictionary to hold cart items
+var cart = {}
+
+// verify our websocket connection is established
+socket.on('connect', function() { console.log('websocket connected!'); });
+
+// send data over `server request` channel
+socket.emit('sale_request', {msg: 'stage_sale'});
+
+function remove() {
+	$cart.append("<div class=\"item\"><h2><b>Item</b></h2><p>"+Math.random().toString(36)+"</p><h3 class=\"ui\">Cost</h3></div>")
+}
+// receive data on `add_response` channel
+socket.on('add_to_cart_response', function(item) {
+		// update to the ui
+		$cart.append('<div class="item blue">' + item.name + ' - ' + item.cost + '</div>');
+
+		// add to checkout dictionary
+		checkout_payload[item.name] = item.cost
+		console.log(item);
+});
+
+// send data over `server request` channel
+function checkout() {
+		socket.emit('checkout_request', {data: checkout_payload});
+}
+
+// receive data on `pay_response` channel
+socket.on('pay_response', function(payload) {
+		// FIXME ui this stuff
+		$checkout.append('<br>');
+		$checkout.append('<div class="ui brown">' + payload.msg + '</div>');
+		console.log(payload);
+});
