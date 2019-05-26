770971c0c5453a6ab54074b9365a9a4d248b8954
Julian <julianlambert@bennington.edu>
2019-05-26 19:11:28 -0400

    moved cart websocket js to cart.js file

---
+++ server/static/cart.js
@@ -0,0 +1,89 @@
+var socket = io.connect('http://' + document.domain + ':' + location.port);
+
+// get cart element
+var $cart = $('#cart');
+
+// cart items dictionary
+var cart = {}
+
+// tutorial toggle
+var tutorial = true;
+
+// connect to the websocket and send to console
+socket.on('connect', function() { console.log('websocket connected!'); });
+
+// send data over `server request` channel
+socket.emit('cart_request', {msg: 'cart_request'});
+
+// receive data on `cart_response` channel
+socket.on('cart_response', function(item) {
+		if (tutorial) {
+			$('#replace').remove();
+			tutorial = false;
+		}
+		// update to the ui
+		$cart.append(
+			"<div class=\"item\" id=\"" + item.index + "\">"
+			+ "<h2>" + item.name + "</h2>"
+			+ "<p>" + item.description + "</p>"
+			+ "<h3 class=\"ui\">" + item.cost + "</h3>" +
+			"</div>"
+		)
+
+		// allow selecting
+		$("#"+item.index).on('click touch', function() {
+			$(this).toggleClass(".remove");
+		});
+
+		// add to cart items dictionary
+		cart[item.index] = item
+		console.log(item);
+});
+
+function remove() {
+	$remove = $(".item.remove");
+	$remove.each(function () {
+		delete cart[$(this).attr("id")];
+		$(this).remove();
+	});
+}
+
+// emulate adding an item
+var index = 1;
+function sim_add() {
+	if (tutorial) {
+		$('#replace').remove();
+		tutorial = false;
+	}
+
+	// update to the ui
+	$cart.append(
+		"<div class=\"item\" id=\"" + index + "\">"
+		+ "<h2>Item</h2>"
+		+ "<p>Description</p>"
+		+ "<h3 class=\"ui\">Cost</h3>" +
+		"</div>"
+	)
+
+	// allow selecting
+	$("#"+index+"").on('click touch', function() {
+		$(this).toggleClass("remove");
+	});
+
+	// add to cart items dictionary
+	cart[index] = "test"
+	console.log(index);
+	index++;
+}
+// send data over `server request` channel
+function checkout() {
+		socket.emit('checkout_request', {data: checkout_payload});
+}
+
+// receive data on `pay_response` channel
+socket.on('pay_response', function(payload) {
+		// FIXME ui this stuff
+		$checkout.append('<br>');
+		$checkout.append('<div class="ui brown">' + payload.msg + '</div>');
+		console.log(payload);
+});
